bd3d7a5731c182d9a7813a234d2b36ae4e4cb767
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2025-08-16 13:38:14 -0700

    Use configurable extension interface (#70)
    * Use ConfigurableExtensionInterface
    
    * Update PhikiExtension.php
    
    * Add test for config
    
    ---------
    
    Co-authored-by: Ryan Chandler <ryangjchandler@gmail.com>

--- src/CommonMark/PhikiExtension.php
+++ src/CommonMark/PhikiExtension.php
@@ -4,26 +4,46 @@ namespace Phiki\CommonMark;
 
 use League\CommonMark\Environment\EnvironmentBuilderInterface;
 use League\CommonMark\Extension\CommonMark\Node\Block\FencedCode;
-use League\CommonMark\Extension\ExtensionInterface;
+use League\CommonMark\Extension\ConfigurableExtensionInterface;
 use Phiki\Phiki;
 use Phiki\Theme\Theme;
+use League\Config\ConfigurationBuilderInterface;
+use Nette\Schema\Expect;
 
-class PhikiExtension implements ExtensionInterface
+class PhikiExtension implements ConfigurableExtensionInterface
 {
     /**
      * @param  bool  $withGutter  Include a gutter in the generated HTML. The gutter typically contains line numbers and helps provide context for the code.
      * @param  bool  $withWrapper  Wrap the generated HTML in an additional `<div>` so that it can be styled with CSS. Useful for avoiding overflow issues.
      */
     public function __construct(
-        private string|array|Theme $theme,
+        private string|array|Theme $theme = Theme::Nord,
         private Phiki $phiki = new Phiki,
         private bool $withGutter = false,
         private bool $withWrapper = false,
     ) {}
 
+    public function configureSchema(ConfigurationBuilderInterface $builder): void
+    {
+        $builder->addSchema('phiki', Expect::structure([
+            'theme' => Expect::mixed()->default($this->theme),
+            'with_gutter' => Expect::bool()->default($this->withGutter),
+            'with_wrapper' => Expect::bool()->default($this->withWrapper),
+        ]));
+    }
+
     public function register(EnvironmentBuilderInterface $environment): void
     {
-        $environment
-            ->addRenderer(FencedCode::class, new CodeBlockRenderer($this->theme, $this->phiki, $this->withGutter, $this->withWrapper), 10);
+        $config = $environment->getConfiguration();
+
+        $theme = $config->get('phiki/theme');
+        $withGutter = $config->get('phiki/with_gutter');
+        $withWrapper = $config->get('phiki/with_wrapper');
+
+        $environment->addRenderer(
+            FencedCode::class,
+            new CodeBlockRenderer($theme, $this->phiki, $withGutter, $withWrapper),
+            10,
+        );
     }
 }
--- tests/Unit/CommonMark/PhikiExtensionTest.php
+++ tests/Unit/CommonMark/PhikiExtensionTest.php
@@ -4,45 +4,67 @@ use League\CommonMark\Environment\Environment;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 use League\CommonMark\MarkdownConverter;
 use Phiki\CommonMark\PhikiExtension;
+use Phiki\Theme\Theme;
 
-describe('CommonMark > Extension', function () {
-    it('registers renderers', function () {
-        $environment = new Environment;
+it('registers renderers', function () {
+    $environment = new Environment;
 
-        $environment
-            ->addExtension(new CommonMarkCoreExtension)
-            ->addExtension(new PhikiExtension('github-dark'));
+    $environment
+        ->addExtension(new CommonMarkCoreExtension)
+        ->addExtension(new PhikiExtension('github-dark'));
 
-        $markdown = new MarkdownConverter($environment);
-        $generated = $markdown->convert(<<<'MD'
-        ```php
-        class A {}
-        ```
-        MD)->getContent();
+    $markdown = new MarkdownConverter($environment);
+    $generated = $markdown->convert(<<<'MD'
+    ```php
+    class A {}
+    ```
+    MD)->getContent();
 
-        expect($generated)
-            ->toContain('phiki')
-            ->toContain('github-dark')
-            ->toContain('<span class="token" style="color: #b392f0;">A</span>');
-    });
+    expect($generated)
+        ->toContain('phiki')
+        ->toContain('github-dark')
+        ->toContain('<span class="token" style="color: #b392f0;">A</span>');
+});
 
-    it('auto detects grammar if not provided', function () {
-        $environment = new Environment;
+it('auto detects grammar if not provided', function () {
+    $environment = new Environment;
 
-        $environment
-            ->addExtension(new CommonMarkCoreExtension)
-            ->addExtension(new PhikiExtension('github-dark'));
+    $environment
+        ->addExtension(new CommonMarkCoreExtension)
+        ->addExtension(new PhikiExtension('github-dark'));
 
-        $markdown = new MarkdownConverter($environment);
-        $generated = $markdown->convert(<<<'MD'
-        ```
-        <?php
+    $markdown = new MarkdownConverter($environment);
+    $generated = $markdown->convert(<<<'MD'
+    ```
+    <?php
 
-        echo "Hello, world!";
-        ```
-        MD)->getContent();
+    echo "Hello, world!";
+    ```
+    MD)->getContent();
 
-        expect($generated)
-            ->toContain('data-language="php"');
-    });
+    expect($generated)
+        ->toContain('data-language="php"');
+});
+
+it('can be configured using environment config array', function () {
+    $environment = new Environment([
+        'phiki' => [
+            'theme' => Theme::GithubLight,
+            'with_gutter' => false,
+            'with_wrapper' => false,
+        ],
+    ]);
+
+    $environment
+        ->addExtension(new CommonMarkCoreExtension)
+        ->addExtension(new PhikiExtension);
+    $markdown = new MarkdownConverter($environment);
+
+    $generated = $markdown->convert(<<<'MD'
+    ```php
+    class A {}
+    ```
+    MD)->getContent();
+
+    expect($generated)->toMatchSnapshot();
 });
