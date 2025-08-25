f2dc705f75382988874ca6908b8ab89f25e34eb7
Ryan Chandler <ryangjchandler@gmail.com>
2025-08-25 02:33:55 +0100

    Add convenience methods for decorations

--- src/Transformers/Decorations/LineDecoration.php
+++ src/Transformers/Decorations/LineDecoration.php
@@ -14,6 +14,18 @@ class LineDecoration
         public ClassList $classes,
     ) {}
 
+    public static function forLine(int $line): self
+    {
+        return new self($line, new ClassList());
+    }
+
+    public function class(string ...$classes): self
+    {
+        $this->classes->add(...$classes);
+        
+        return $this;
+    }
+
     public function appliesToLine(int $line): bool
     {
         return $this->line === $line || (is_array($this->line) && $line >= $this->line[0] && $line <= $this->line[1]);
